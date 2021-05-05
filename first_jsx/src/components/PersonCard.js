862c4aa9491a68480c0a3e6882f4d7304f471a76
Maxine Causevic <maxine@fitmaxpersonaltraining.com>
2021-05-05 16:17:19 -0500

    birthdaybutton

--- first_jsx/src/components/PersonCard.js
+++ first_jsx/src/components/PersonCard.js
@@ -1,13 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import App from '../App';
 
-class PersonCard extends Component{
-    render(){
-        return(
+class PersonCard extends Component {
+    constructor(props) {
+        console.log(props)
+        super(props);
+        this.state = {
+            birthday: this.props.age
+        };
+    }
+    birthdayButton = () => {
+        // this.age += 1;
+        console.log("Happy Birthday", this.props.firstName)
+        //this is how we alter state
+        this.setState({
+            birthday: this.state.birthday +1 
+
+        })
+    }
+    render() {
+        return (
             <>
-            <h1>{this.props.firstName} {this.props.lastName}</h1>
-            <p>Age:{this.props.age}</p>
-            <p>Hair color:{this.props.hairColor}</p>
+                <h1>{this.props.firstName} {this.props.lastName}</h1>
+                <p>Age:{this.state.birthday}</p>
+                <p>Hair color:{this.props.hairColor}</p>
+                <button onClick={this.birthdayButton}>Happy Birthday</button>
             </>
         )
     }
